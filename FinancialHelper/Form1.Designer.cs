1a5883188d9796fdf611807f336124feed0621a6
David Mihajlović <dmihajlovic@etfos.hr>
2024-03-29 11:35:08 +0100

    Add project files.

---
+++ FinancialHelper/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace FinancialHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(107, 165);
+            button1.Name = "button1";
+            button1.Size = new Size(183, 101);
+            button1.TabIndex = 0;
+            button1.Text = "Credit calculator";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(509, 165);
+            button2.Name = "button2";
+            button2.Size = new Size(183, 101);
+            button2.TabIndex = 1;
+            button2.Text = "Savings calculator";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(284, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(214, 33);
+            textBox1.TabIndex = 2;
+            textBox1.Text = "Select desired calculator";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+    }
+}
